bf506bfc9811ff56661bb868c9a3b44c8fe99d47
Zainab Omolola Fadeyi <70825458+Zainabfadeyi@users.noreply.github.com>
2026-03-22 12:29:10 +0100

    commit

---
+++ src/WeekOneAssignment/BackToSenderLogistics.java
@@ -0,0 +1,24 @@
+package src.WeekOneAssignment;
+
+public class BackToSenderLogistics {
+    public static int calculateRiderWage(int successfulDeliveries) {
+        final int BASE_PAY = 5000;
+        final int TOTAL_PACKAGES = 100;
+
+        double collectionRate = (successfulDeliveries / (double) TOTAL_PACKAGES) * 100;
+
+        int amountPerParcel;
+
+        if (collectionRate < 50) {
+            amountPerParcel = 160;
+        } else if (collectionRate <= 59) {
+            amountPerParcel = 200;
+        } else if (collectionRate <= 69) {
+            amountPerParcel = 250;
+        } else {
+            amountPerParcel = 500;
+        }
+
+        return successfulDeliveries * amountPerParcel + BASE_PAY;
+    }
+}
